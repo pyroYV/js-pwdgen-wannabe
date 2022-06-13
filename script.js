673862a5ffffaba81a88e51a46027c821b1e68e1
pyroYV <simonerizzo.pvt@gmail.com>
2022-06-13 15:30:27 +0200

    super bonus

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const userName = prompt('Please enter your name');
 const userSurname = prompt('Please enter your surname');
 
 const userColor = prompt('Please enter your favourite color in ENGLISH');
+const userColor2 = prompt('Please enter your favourite least color in ENGLISH');
 
 // stampa variabile su html id=pssw
 document.getElementById("pssw").innerHTML = userName + userSurname + userColor;
@@ -13,10 +14,17 @@ document.getElementById("pssw").innerHTML = userName + userSurname + userColor;
 const userNameTrimmed = userName.replaceAll(/\s/g,'');
 const userSurnameTrimmed = userSurname.replaceAll(/\s/g,'');
 const userColorTrimmed = userColor.replaceAll(/\s/g,'');
-console.log(userNameTrimmed);
+
 
 document.getElementById("changing-color-div").style.backgroundColor = userColor;
+document.getElementById("changing-color-div2").style.backgroundColor = userColor;
+
+let leastColor = document.getElementsByClassName('changing-div')
+for(i = 0; i < leastColor.length; i++) {
+    leastColor[i].style.backgroundColor = userColor2 ;
+  }
 
 
-document.getElementById("pssw2").innerHTML = userNameTrimmed + userSurnameTrimmed + userColorTrimmed;
-console.log(userNameTrimmed,userSurnameTrimmed,userColorTrimmed)
+let finalPssw = document.getElementById("pssw2").innerHTML = userNameTrimmed + userSurnameTrimmed + userColorTrimmed;
+document.getElementById("pssw3").innerHTML = userNameTrimmed + userSurnameTrimmed + userColorTrimmed;
+console.log(finalPssw)
